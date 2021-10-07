e5d1c48a40e716125d83c326d4a767c0bf4ce958
dcaled <dcaled@gmail.com>
2021-10-07 19:24:20 +0100

    Add a new endpoint to retrieve ERC data to a given registration number.

--- inforadar/__init__.py
+++ inforadar/__init__.py
@@ -6,6 +6,7 @@ from inforadar.endpoints.metadata import Metadata
 from inforadar.endpoints.metrics import Metrics
 from inforadar.endpoints.scrapper import Scraper
 from inforadar.endpoints.source_checker import SourceChecker
+from inforadar.endpoints.source_info import SourceInfo
 
 api = Api(app)
 
@@ -14,7 +15,7 @@ api.add_resource(Metrics, '/api2/metrics')
 api.add_resource(Scraper, '/api2/scrapper')
 api.add_resource(Metadata, '/api2/metadata')
 api.add_resource(SourceChecker, '/api2/source_checker')
-
+api.add_resource(SourceInfo, '/api2/source_info')
 
 if __name__ == '__main__':
     app.run()
