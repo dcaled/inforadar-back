c6873a9751ed0964215fb4640e8cfcf0500cc678
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2021-12-26 20:33:14 +0000

    Add condition to examples to rule out articles not analysed

--- inforadar/endpoints/examples.py
+++ inforadar/endpoints/examples.py
@@ -1,13 +1,16 @@
 from flask_restful import Resource
 
-from inforadar.models import CrowdsourcedArticleSchema, CrowdsourcedArticle
+from inforadar.models import CrowdsourcedArticleSchema, CrowdsourcedArticle, CrowdsourcedIndicatorScore
 
 
 class Examples(Resource):
     def get(self):
         # Create the list of csarticles from our data
-        csarticle = CrowdsourcedArticle.query.filter(
-            CrowdsourcedArticle.erc_registered == True).order_by(CrowdsourcedArticle.id.desc()).limit(3)
+        csarticle = CrowdsourcedArticle .query \
+                                        .join(CrowdsourcedIndicatorScore, CrowdsourcedIndicatorScore.crowdsourced_article_id == CrowdsourcedArticle.id) \
+                                        .group_by(CrowdsourcedArticle.id) \
+                                        .filter(CrowdsourcedArticle.erc_registered == True) \
+                                        .order_by(CrowdsourcedArticle.id.desc()).limit(3).all()
 
         # Serialize the data for the response
         csarticle_schema = CrowdsourcedArticleSchema(many=True)
