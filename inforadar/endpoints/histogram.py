9fa35f26957b804f861ca8d743a28792598033f0
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2022-01-19 15:20:07 +0000

    Remove the bins generation for the js histogram

--- inforadar/endpoints/histogram.py
+++ inforadar/endpoints/histogram.py
@@ -11,7 +11,6 @@ import seaborn as sns
 from scipy import stats
 import matplotlib.pyplot as plt
 from matplotlib import colors
-from sqlalchemy.sql.expression import func
 import inforadar.config as config
 from inforadar.models import Category, Metric, CorpusMetricScore, CorpusArticle
 
@@ -56,32 +55,14 @@ class Histogram(Resource):
                 continue
             metric_bins[metric_id] = {"categories": dict()}
             for category_id in categories.keys():
-                # Create the list of bins from our data
-                pos_bins = config.db.session \
-                    .query((func.floor(CorpusMetricScore.score / 0.001) * 0.001).label("floor"),
-                           func.count(CorpusMetricScore.id).label("count")) \
-                    .join(CorpusArticle, CorpusArticle.id == CorpusMetricScore.corpus_article_id) \
-                    .filter(CorpusMetricScore.metric_id == metric_id) \
-                    .filter(CorpusArticle.category_id == category_id) \
-                    .group_by("floor") \
-                    .order_by("floor")
-
-                neg_bins = config.db.session \
-                    .query((func.floor(CorpusMetricScore.score / 0.001) * 0.001).label("floor"),
-                           func.count(CorpusMetricScore.id).label("count")) \
-                    .join(CorpusArticle, CorpusArticle.id == CorpusMetricScore.corpus_article_id) \
-                    .filter(CorpusMetricScore.metric_id == metric_id) \
-                    .filter(CorpusArticle.category_id != category_id) \
-                    .group_by("floor") \
-                    .order_by("floor")
-
-                bins = config.db.session \
+                # Filter our data
+                metricdata = config.db.session \
                     .query(CorpusMetricScore.score, CorpusArticle.category_id, CorpusMetricScore.metric_id) \
                     .join(CorpusArticle, CorpusArticle.id == CorpusMetricScore.corpus_article_id) \
                     .filter(CorpusMetricScore.metric_id == metric_id) \
                     .order_by(CorpusMetricScore.score)
 
-                df = pd.read_sql(bins.statement, bins.session.bind)
+                df = pd.read_sql(metricdata.statement, metricdata.session.bind)
                 df['categoria'] = np.where(
                     df['category_id'] == category_id, f'categoria {category_id}', 'outras')
                 sns.set_theme(style="whitegrid")
@@ -101,38 +82,16 @@ class Histogram(Resource):
                     if patch.get_x() <= 0.05 < patch.get_x() + patch.get_width() and colors.to_hex(patch.get_facecolor()) == '#00539d':
                         patch.set_facecolor('#f4664a')
 
-                a = stats.ks_2samp(df.loc[df['category_id'] == category_id]['score'],
-                                   df.loc[df['category_id'] != category_id]['score'])
+                ks = stats.ks_2samp(df.loc[df['category_id'] == category_id]['score'],
+                                    df.loc[df['category_id'] != category_id]['score'])
                 plt.suptitle(
-                    f'{round(a.statistic, 3)}, p={round(a.pvalue, 5)}')
+                    f'{round(ks.statistic, 3)}, p={round(ks.pvalue, 5)}')
                 plotoutput = io.StringIO()
                 plt.savefig(plotoutput, format='svg')
                 plt.close()
-                #f = open(f'c{category_id}m{metric_id}.svg', 'w')
-                # f.write(plotoutput.getvalue())
-                # f.close()
 
-                # select
-                #   floor(actions_count/100.00)*100 as bin_floor,
-                #   count(user_id) as count
-                # from product_actions
-                # group by bin_floor
-                # order by bin_floor;
-
-                metric_bin_pos = []
-                metric_bin_neg = []
-                for b in pos_bins.all():
-                    metric_bin_pos.append({
-                        "count": b.count,
-                        "value": b.floor
-                    })
-                for b in neg_bins.all():
-                    metric_bin_neg.append({
-                        "count": b.count,
-                        "value": b.floor
-                    })
                 metric_bins[metric_id]["categories"][category_id] = {
-                    "pos": metric_bin_pos, "neg": metric_bin_neg, "svg": plotoutput.getvalue()}
+                    "svg": plotoutput.getvalue(), "ks_2samp": {"stat": ks.statistic, "p": ks.pvalue}}
                 plotoutput.close()
 
         return metric_bins, 200
