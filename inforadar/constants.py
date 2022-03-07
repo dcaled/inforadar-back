6b9cf179651771ba3e82b64ac58e3f3cdbe08e06
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2022-03-07 17:10:25 +0000

    Make constants relative

--- inforadar/constants.py
+++ inforadar/constants.py
@@ -29,4 +29,25 @@ fp_lex_sent = "{}processed/sentiment.pkl".format(base_dir)
 # fp_lex_raw_subj = '{}subjectivity-clues-pt.csv'.format(base_dir)
 fp_lex_subj = "{}processed/subjectivity.pkl".format(base_dir)
 
+# Embeddings path
 fp_emb_matrix = "{}cbow_s300.txt".format(base_dir)
+
+# Explainable rules
+fp_explainable_rules = "inforadar/util/explainable_rules.json"
+
+# Clickbait models
+fp_clickbait_vectorizer = "inforadar/util/clickbait_vectorizer.pk"
+fp_clickbait_model = "inforadar/util/clickbait_model.sav"
+
+# Histograms cache
+cache_histograms = "inforadar/util/histocache.json"
+
+# Indicators current version
+current_version_indicator_1 = 1
+
+# Metrics current version
+current_version_metric_clickbait = 1
+current_version_metric_sentiment = 1
+current_version_metric_subjectivity = 1
+current_version_metric_spell_checking = 1
+current_version_metric_headline_accuracy = 1
