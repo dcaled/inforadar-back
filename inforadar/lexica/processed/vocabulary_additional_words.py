0757fc1beebbe534e2091bf82b238238b93e132a
dcaled <dcaled@gmail.com>
2022-03-14 13:58:41 +0000

    Delete file.

--- inforadar/lexica/processed/vocabulary_additional_words.py
+++
@@ -1,5 +0,0 @@
-additional_words = [
-    "marcelo", "rebelo", "covid", "covid-19", "oms", "sars-cov", "pfizer",
-    "bolsonaro", "covid-19", "https", "huawei", "coronavírus", "5g", "biden", "hidroxicloroquina",
-    "lisboa",
-    "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira"]
