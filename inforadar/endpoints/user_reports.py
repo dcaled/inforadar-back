ab5e16b60e9a76e73ea7bb760250f71c621751d7
Francisco Sousa <francisco.sousa.98@tecnico.ulisboa.pt>
2023-04-17 20:30:30 +0000

    Remove exclusive annotator filter from user reports

--- inforadar/endpoints/user_reports.py
+++ inforadar/endpoints/user_reports.py
@@ -13,7 +13,7 @@ class UserReports(Resource):
         if (not current_user.admin):
             return {'message': "Only accessible to admins."}, 403
 
-        users = User.query.order_by(User.id).filter(User.annotator == True)
+        users = User.query.order_by(User.id)
 
         result = []
 
@@ -32,6 +32,7 @@ class UserReports(Resource):
                 'id': user.id,
                 'email': user.email,
                 'name': user.name,
+                'annotator': user.annotator,
                 'articles': str(articles.sum_articles),
                 'avg_time_taken': str(articles.avg_time_taken),
                 'collection': article_collections_strings[user.collection],
